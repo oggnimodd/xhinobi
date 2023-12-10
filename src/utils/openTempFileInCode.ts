182c0476c1b78dc26b0aed7843dd8c3cc92b993f
oggnimodd <arnold.ds.dev@gmail.com>
2023-12-10 23:09:27 +0700

    support google cloudshell

--- src/utils/openTempFileInCode.ts
+++ src/utils/openTempFileInCode.ts
@@ -1,5 +1,9 @@
+import { IS_GOOGLE_CLOUDSHELL } from "@/constants";
+
 const openTempFileInCode = (file: string) => {
-  const proc = Bun.spawn(["code", file]);
+  const proc = IS_GOOGLE_CLOUDSHELL
+    ? Bun.spawn(["cloudshell", "open", file])
+    : Bun.spawn(["code", file]);
   return proc;
 };
 
