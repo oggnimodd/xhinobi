07f30cf4c351ab50f9c90ccceb0938910be22f89
oggnimodd <arnold.ds.dev@gmail.com>
2023-12-10 16:45:34 +0000

    fix cloudshell

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,5 +3,5 @@ export const IS_CODESPACE =
   !!process.env.CODESPACE_NAME && !!process.env.CLOUDENV_ENVIRONMENT_ID;
 export const IS_GOOGLE_CLOUDSHELL =
   !!process.env.DEVSHELL_GCLOUD_CONFIG || !!process.env.BASHRC_GOOGLE_PATH;
-export const IS_CLOUD_ENV = IS_GITPOD || IS_CODESPACE;
+export const IS_CLOUD_ENV = IS_GITPOD || IS_CODESPACE || IS_GOOGLE_CLOUDSHELL;
 export const TEMP_FILE_NAME = "temp_file.txt";
--- src/utils/createTempFile.ts
+++ src/utils/createTempFile.ts
@@ -1,10 +1,14 @@
-import { TEMP_FILE_NAME } from "@/constants";
+import { IS_CLOUD_ENV, TEMP_FILE_NAME } from "@/constants";
 import { existsSync, unlinkSync } from "fs";
-import { tmpdir } from "os";
+import { tmpdir, homedir } from "os";
 import { join } from "path";
 
 const createTempFile = (text: string) => {
-  const tempFilePath = join(tmpdir(), TEMP_FILE_NAME);
+  // In google cloudshell we can only open files that exist in the home directory
+  const tempFilePath = IS_CLOUD_ENV
+    ? join(homedir(), TEMP_FILE_NAME)
+    : join(tmpdir(), TEMP_FILE_NAME);
+
   if (existsSync(tempFilePath)) {
     unlinkSync(tempFilePath);
   }
